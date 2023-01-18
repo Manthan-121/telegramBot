fafda718809ff7bc26b4e135378396d4d7205f6a
“Manthan-121” <computergyanhindi121@gmail.com>
2023-01-18 18:55:33 +0530

    bot is working this time save

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,47 +1,19 @@
-import telegram.ext
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
-Token = "5851226941:AAFkHcNsmSTaweIAfnfT4Me9kFTED9PFVy0"
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
 
-updater = telegram.ext.updater(Token ,use_context=True)
-dispacher = updater.dispacher
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-def start(update,context):
-    update.message.reply_text("Hello! Welcome to this Bot")
-
-def help(update,context):
-    update.message.reply_text(
-        """
-        /start -> Welcomt to this bot
-        /help -> This perticular message
-        /content -> About this bot relatred to content disply
-        /python -> The first video from youtube
-        /SQL -> The first video from youtube
-        /Java -> The first video from youtube
-        /contect -> you can redirect from website
-        """
-    )
-def content(update,context):
-    update.message.reply_text("We have give the varius project")
-
-def python(update,context):
-    update.message.reply_text("You can chose the python")
-
-def SQL(update,context):
-    update.message.reply_text("You can chose the SQL")
-
-def Java(update,context):
-    update.message.reply_text("You can chose the JAVA")
-
-def contect(update,context):
-    update.message.reply_text("You can register using your ID")
-
-dispacher.add_handler(telegram.ext.CommandHandler('start',start))
-dispacher.add_handler(telegram.ext.CommandHandler('start',help))
-dispacher.add_handler(telegram.ext.CommandHandler('start',content))
-dispacher.add_handler(telegram.ext.CommandHandler('start',python))
-dispacher.add_handler(telegram.ext.CommandHandler('start',SQL))
-dispacher.add_handler(telegram.ext.CommandHandler('start',Java))
-dispacher.add_handler(telegram.ext.CommandHandler('start',contect))
-
-updater.star_polling()
-updater.idle()
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('5851226941:AAFkHcNsmSTaweIAfnfT4Me9kFTED9PFVy0').build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
